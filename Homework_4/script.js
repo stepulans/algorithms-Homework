9c9b79c8bf1cdd40cd0dc203e4c0ce92f669ab3a
stepulans <123463857+stepulans@users.noreply.github.com>
2023-06-20 22:43:11 +0200

    Update script.js

--- Homework_4/script.js
+++ Homework_4/script.js
@@ -1,14 +1,14 @@
-function findKthElement(array1, array2, k) {
-    const mergedArray = array1.concat(array2);
-    mergedArray.sort((a, b) => a - b);
-    console.log(mergedArray);
-    return mergedArray[k - 1];
+function findKElement(array1, array2, k) {
+    const newArray = array1.concat(array2);
+    newArray.sort((a, b) => a - b);
+    console.log(newArray);
+    return newArray[k - 1];
   }
   
   const array1 = [100, 112, 256, 349, 770];
   const array2 = [72, 86, 113, 119, 265, 445, 892];
   const k = 7;
   
-  const result = findKthElement(array1, array2, k);
+  const result = findKElement(array1, array2, k);
   console.log(result);
   
